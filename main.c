07ce9103a81e9ad02983add1ed868cd8eb2144e8
codeherlife <codeherlife@gmail.com>
2018-09-03 14:41:36 -0700

    send output to txt file

--- main.c
+++ main.c
@@ -2,11 +2,17 @@
 //  main.c
 //  SpeedLab
 //
-//  Created by Melanie Mills on 8/30/18.
-//  Student id: 012090935
+//  Student Name: Melanie Summers;
+//  Student id: 012090935;
+//  Name of the assignment: Speed Lab;
+//  Date: 8/31/18;
+//  Name and version of the C Compiler: Xcode v. 9.4.1;
+//  Name of Operating System: Mac OS High Sierra v. 10.13.6;
 //
 
+
 #include <stdio.h>
+FILE *fp;
 
 int main(void) {
     float distanceInMiles = 425.5;
@@ -15,14 +21,17 @@ int main(void) {
     float numSecondsTraveled = 3600 * numHoursOfTravel;
     float carsMilesPerHour;
     float carsMetersPerSecond;
+    fp = fopen("csis.txt", "w");
     
     carsMilesPerHour = distanceInMiles / numHoursOfTravel;
-    
     carsMetersPerSecond = metersTraveled / numSecondsTraveled;
     
-    printf("The speed of the car in miles is: %f\n", carsMilesPerHour);
+    printf("The speed of the car in miles per hour is: %.2f\n", carsMilesPerHour);
+    fprintf(fp, "The speed of the car in miles per hour is: %.2f\n", carsMilesPerHour);
     
-    printf("The speed of the car in meters per second is: %f\n", carsMetersPerSecond);
-
+    printf("The speed of the car in meters per second is: %.2f\n", carsMetersPerSecond);
+    fprintf(fp, "The speed of the car in meters per second is: %.2f\n", carsMetersPerSecond);
+    
+    fclose(fp);
     return 0;
 }
